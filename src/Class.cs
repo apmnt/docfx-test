27164d7ffdfa5e80283f93ce3892ca2bee67338f
apmnt <aapomo@gmail.com>
2025-09-08 16:47:22 +0300

    Init

---
+++ src/Class.cs
@@ -0,0 +1,14 @@
+/// <summary>
+/// This is a class
+/// </summary>
+public class Class
+{
+
+    /// <summary>
+    /// This is a method.
+    /// </summary>
+    public void Method()
+    {
+        // Some code here
+    }
+}
